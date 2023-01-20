9cd2a0ca4eb23208945cc306ed96128b81f7f6e0
Maciej Kwaśniak <contact@exelord.com>
2023-01-20 20:01:00 +0100

    Add job

---
+++ src/job.ts
@@ -0,0 +1,134 @@
+import { createObject } from "solid-proxies";
+import { Task } from "./task";
+
+export type TaskFunction<T, Args extends unknown[]> = (
+  signal: AbortSignal,
+  ...args: Args
+) => Promise<T>;
+
+export enum JobStatus {
+  Idle = "idle",
+  Pending = "pending",
+}
+
+export enum JobMode {
+  Drop = "drop",
+  Restart = "restart",
+}
+
+export interface JobOptions {
+  mode?: JobMode;
+}
+
+interface ReactiveState<T> {
+  status: JobStatus;
+  performCount: number;
+  lastPending?: Task<T>;
+  lastFulfilled?: Task<T>;
+  lastRejected?: Task<T>;
+  lastSettled?: Task<T>;
+}
+
+export class Job<T, Args extends unknown[]> {
+  get status(): ReactiveState<T>["status"] {
+    return this.#reactiveState.status;
+  }
+
+  get isIdle(): boolean {
+    return this.status === JobStatus.Idle;
+  }
+
+  get isPending(): boolean {
+    return this.status === JobStatus.Pending;
+  }
+
+  get lastPending(): ReactiveState<T>["lastPending"] {
+    return this.#reactiveState.lastPending;
+  }
+
+  get lastFulfilled(): ReactiveState<T>["lastFulfilled"] {
+    return this.#reactiveState.lastFulfilled;
+  }
+
+  get lastRejected(): ReactiveState<T>["lastRejected"] {
+    return this.#reactiveState.lastRejected;
+  }
+
+  get lastSettled(): ReactiveState<T>["lastSettled"] {
+    return this.#reactiveState.lastSettled;
+  }
+
+  get performCount(): ReactiveState<T>["performCount"] {
+    return this.#reactiveState.performCount;
+  }
+
+  #taskFn: TaskFunction<T, Args>;
+  #options: JobOptions;
+
+  #reactiveState: ReactiveState<T> = createObject({
+    status: JobStatus.Idle,
+    performCount: 0,
+  });
+
+  constructor(taskFn: TaskFunction<T, Args>, options: JobOptions = {}) {
+    options.mode ??= JobMode.Drop;
+    this.#taskFn = taskFn;
+    this.#options = options;
+  }
+
+  perform(...args: Args): Task<T> {
+    const task = new Task<T>((signal) => this.#taskFn(signal, ...args));
+    this.#reactiveState.performCount++;
+
+    if (this.lastPending) {
+      if (this.#options.mode === JobMode.Drop) {
+        task.cancel();
+        return task;
+      }
+
+      if (this.#options.mode === JobMode.Restart) {
+        this.lastPending.cancel();
+      }
+    }
+
+    this.#performTask(task);
+    return task;
+  }
+
+  async cancelAll(reason?: string): Promise<void> {
+    await this.lastPending?.cancel(reason);
+  }
+
+  #performTask(task: Task<T>): void {
+    task
+      .perform()
+      .then(
+        () => {
+          this.#reactiveState.lastFulfilled = task;
+        },
+        () => {
+          if (task.isRejected) this.#reactiveState.lastRejected = task;
+        }
+      )
+      .finally(() => {
+        if (task.isFulfilled || task.isRejected) {
+          this.#reactiveState.lastSettled = task;
+        }
+
+        if (this.#reactiveState.lastPending === task) {
+          this.#reactiveState.lastPending = undefined;
+          this.#reactiveState.status = JobStatus.Idle;
+        }
+      });
+
+    this.#reactiveState.lastPending = task;
+    this.#reactiveState.status = JobStatus.Pending;
+  }
+}
+
+export function createJob<T, Args extends unknown[]>(
+  taskFn: TaskFunction<T, Args>,
+  options: JobOptions = {}
+): Job<T, Args> {
+  return new Job(taskFn, options);
+}
---
+++ tests/vitest/job.test.ts
@@ -0,0 +1,101 @@
+import { work, timeout } from "../../src/work";
+import { createJob, JobMode } from "../../src/job";
+import { describe, test, expect } from "vitest";
+
+describe("job", () => {
+  describe("#perform", () => {
+    test("drop", async () => {
+      const job = createJob(async (signal) => {
+        await timeout(signal, 1);
+        return "Hello World";
+      });
+
+      expect(job.performCount).toBe(0);
+      expect(job.isIdle).toBe(true);
+
+      const task1 = job.perform();
+
+      expect(job.performCount).toBe(1);
+      expect(job.isPending).toBe(true);
+      expect(job.lastPending).toBe(task1);
+
+      const task2 = job.perform();
+
+      expect(job.performCount).toBe(2);
+      expect(job.isPending).toBe(true);
+      expect(job.lastPending).toBe(task1);
+
+      await job.lastPending;
+
+      expect(job.isIdle).toBe(true);
+      expect(task1.isFulfilled).toBe(true);
+      expect(task2.isDropped).toBe(true);
+
+      expect(job.lastFulfilled).toBe(task1);
+      expect(job.lastSettled).toBe(task1);
+      expect(job.lastRejected).toBe(undefined);
+      expect(job.lastPending).toBe(undefined);
+    });
+
+    test("restart", async () => {
+      const job = createJob(
+        async (signal) => {
+          await timeout(signal, 1);
+          return "Hello World";
+        },
+        { mode: JobMode.Restart }
+      );
+
+      expect(job.performCount).toBe(0);
+      expect(job.isIdle).toBe(true);
+
+      const task1 = job.perform();
+
+      expect(job.performCount).toBe(1);
+      expect(job.isPending).toBe(true);
+      expect(job.lastPending).toBe(task1);
+
+      const task2 = job.perform();
+
+      expect(job.performCount).toBe(2);
+      expect(job.isPending).toBe(true);
+      expect(job.lastPending).toBe(task2);
+
+      await job.lastPending;
+
+      expect(job.isIdle).toBe(true);
+      expect(task1.isCancelled).toBe(true);
+      expect(task2.isFulfilled).toBe(true);
+
+      expect(job.performCount).toBe(2);
+      expect(job.lastFulfilled).toBe(task2);
+      expect(job.lastSettled).toBe(task2);
+      expect(job.lastRejected).toBe(undefined);
+      expect(job.lastPending).toBe(undefined);
+    });
+  });
+
+  test("#cancelAll", async () => {
+    const job = createJob(async (signal) => {
+      await work(signal, new Promise(() => {}));
+    });
+
+    expect(job.performCount).toBe(0);
+    expect(job.isIdle).toBe(true);
+
+    const task = job.perform();
+
+    expect(job.performCount).toBe(1);
+    expect(job.isPending).toBe(true);
+
+    await job.cancelAll();
+
+    expect(job.isIdle).toBe(true);
+    expect(task.isCancelled).toBe(true);
+
+    expect(job.lastFulfilled).toBe(undefined);
+    expect(job.lastSettled).toBe(undefined);
+    expect(job.lastRejected).toBe(undefined);
+    expect(job.lastPending).toBe(undefined);
+  });
+});
