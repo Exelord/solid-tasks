60e1e6b9848d88ba578a54c1e22c0be39075dd66
Maciej Kwaśniak <contact@exelord.com>
2023-02-16 11:27:36 +0100

    Unsubscribe from signal after race

--- src/work.ts
+++ src/work.ts
@@ -1,18 +1,38 @@
+function abortablePromise<T>(signal: AbortSignal) {
+  let reject!: (reason?: any) => void;
+
+  const promise = new Promise<T>((_, reject_) => {
+    reject = reject_;
+  });
+
+  const callback = () => {
+    reject(signal.reason);
+  };
+
+  signal.addEventListener("abort", callback, {
+    once: true,
+    passive: true,
+  });
+
+  return {
+    promise,
+    abort(): void {
+      signal.removeEventListener("abort", callback);
+      reject();
+    },
+  };
+}
+
 export async function work<T>(signal: AbortSignal, promise: Promise<T>) {
   signal.throwIfAborted();
 
-  return Promise.race([
-    new Promise<never>((_resolve, reject) => {
-      signal.addEventListener(
-        "abort",
-        () => {
-          reject(signal.reason);
-        },
-        { once: true, passive: true }
-      );
-    }),
-    promise,
-  ]);
+  const controlledPromise = abortablePromise<never>(signal);
+
+  try {
+    return await Promise.race([controlledPromise.promise, promise]);
+  } finally {
+    controlledPromise.abort();
+  }
 }
 
 export async function timeout(signal: AbortSignal, ms: number): Promise<void> {
